673eff9609dec989c94b2d1d6eaefd3df2b3a6bf
evan tahler <evantahler@gmail.com>
2018-07-15 15:23:28 -0700

    orginization

--- examples/client.js
+++ examples/client.js
@@ -6,49 +6,49 @@ const Hyperjob = require(path.join('..', 'index.js'))
 const name = process.argv[2] || 'client'
 const archiveKey = process.argv[3]
 
-const hyperjob = new Hyperjob({
+const producer = new Hyperjob.Producer({
   archiveKey: archiveKey,
   archivePath: `./test_data/${name}`
 })
 
-hyperjob.on('ready', (version) => { console.log(`Ready! (version: ${version})`) })
-hyperjob.on('connecting', () => { console.log(`connecting to swarm...`) })
-hyperjob.on('error', (error) => { console.error(`Error: ${error}`) })
-hyperjob.on('archiveKey', (key) => { console.log(`Archive Key: ${key}`) })
-hyperjob.on('peerConnect', (peer, type) => { console.log(`Peer Connected: ${type.host}:${type.port} (${type.type})`) })
-hyperjob.on('peerDisconnect', (peer, type) => { console.log(`Peer Disconnected: ${type.host}:${type.port} (${type.type})`) })
+producer.on('ready', (version) => { console.log(`Ready! (version: ${version})`) })
+producer.on('connecting', () => { console.log(`connecting to swarm...`) })
+producer.on('error', (error) => { console.error(`Error: ${error}`) })
+producer.on('archiveKey', (key) => { console.log(`Archive Key: ${key}`) })
+producer.on('peerConnect', (peer, type) => { console.log(`Peer Connected: ${type.host}:${type.port} (${type.type})`) })
+producer.on('peerDisconnect', (peer, type) => { console.log(`Peer Disconnected: ${type.host}:${type.port} (${type.type})`) })
 
 async function main () {
-  await hyperjob.connect()
+  await producer.connect()
 
   // add a job
-  await hyperjob.enqueue('low_priority', 'doSomeMath', {a: 1, b: 2})
+  await producer.enqueue('low_priority', 'doSomeMath', {a: 1, b: 2})
 
   // see the queue
-  let queues = await hyperjob.queues()
+  let queues = await producer.queues()
   console.log(queues)
   console.log(`Queues: ${queues}`)
 
   // list enqueued jobs
-  let jobs = await hyperjob.enqueued(queues[0])
+  let jobs = await producer.enqueued(queues[0])
   console.log(`Jobs: ${jobs}`)
 
   // // inspect the job
-  let job = await hyperjob.get(queues[0], jobs[0])
+  let job = await producer.get(queues[0], jobs[0])
   console.log(`Job: ${JSON.stringify(job)}`)
 
   // // delete the job
-  await hyperjob.del(queues[0], jobs[0])
-  jobs = await hyperjob.enqueued(queues[0])
+  await producer.del(queues[0], jobs[0])
+  jobs = await producer.enqueued(queues[0])
   console.log(`Jobs (after single delete): ${jobs}`)
 
   // // delete the queue
-  await hyperjob.enqueue('low-priority', 'doSomeMath', {a: 1, b: 2})
-  await hyperjob.delQueue(queues[0])
-  jobs = await hyperjob.enqueued(queues[0])
+  await producer.enqueue('low-priority', 'doSomeMath', {a: 1, b: 2})
+  await producer.delQueue(queues[0])
+  jobs = await producer.enqueued(queues[0])
   console.log(`Jobs (after queue delete): ${jobs}`)
 
-  await hyperjob.close()
+  await producer.close()
   console.log('~~ all done! ~~')
 }
 
--- examples/idle.js
+++ examples/idle.js
@@ -6,20 +6,20 @@ const Hyperjob = require(path.join('..', 'index.js'))
 const name = process.argv[2] || 'client'
 const archiveKey = process.argv[3]
 
-const hyperjob = new Hyperjob({
+const producer = new Hyperjob.Producer({
   archiveKey: archiveKey,
   archivePath: `./test_data/${name}`
 })
 
-hyperjob.on('ready', (version) => { console.log(`Ready! (version: ${version})`) })
-hyperjob.on('connecting', () => { console.log(`connecting to swarm...`) })
-hyperjob.on('error', (error) => { console.error(`Error: ${error}`) })
-hyperjob.on('archiveKey', (key) => { console.log(`Archive Key: ${key}`) })
-hyperjob.on('peerConnect', (peer, type) => { console.log(`Peer Connected: ${type.host}:${type.port} (${type.type})`) })
-hyperjob.on('peerDisconnect', (peer, type) => { console.log(`Peer Disconnected: ${type.host}:${type.port} (${type.type})`) })
+producer.on('ready', (version) => { console.log(`Ready! (version: ${version})`) })
+producer.on('connecting', () => { console.log(`connecting to swarm...`) })
+producer.on('error', (error) => { console.error(`Error: ${error}`) })
+producer.on('archiveKey', (key) => { console.log(`Archive Key: ${key}`) })
+producer.on('peerConnect', (peer, type) => { console.log(`Peer Connected: ${type.host}:${type.port} (${type.type})`) })
+producer.on('peerDisconnect', (peer, type) => { console.log(`Peer Disconnected: ${type.host}:${type.port} (${type.type})`) })
 
 async function main () {
-  await hyperjob.connect()
+  await producer.connect()
 }
 
 main()
